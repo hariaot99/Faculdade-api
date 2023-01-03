4ca34d7af15478d9106606e49b935dd95fdaf790
Hariã Othon <haria.othon@unesp.br>
2023-01-03 07:54:48 -0300

    initial commit

---
+++ src/main/java/faculdade/sys_faculdade/SysFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package faculdade.sys_faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysFaculdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/faculdade/sys_faculdade/SysFaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package faculdade.sys_faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysFaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
